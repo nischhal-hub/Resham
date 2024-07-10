6378e51017657cda7231e3da7fb4649d62352673
bishal-Timilsina <sharmavai33@gmail.com>
2024-07-10 19:15:50 +0545

    sync

--- project/client/src/components/pages/Report.jsx
+++ project/client/src/components/pages/Report.jsx
@@ -3,6 +3,9 @@ import UserTable from '@/features/users/UserTable';
 export default function Report() {
   
   return (
-    <div><UserTable/></div>
+    
+    <div className='w-full ml-64 min-h-screen'>
+      <UserTable/>
+    </div>
   )
 }
