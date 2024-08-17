45cae58dc304afb80b3669bc1c5807191a12c369
nischhal-nub <nischalbh51@gmail.com>
2024-08-17 17:25:50 +0545

    add .env and configured it

--- project/client/src/services/supabase.js
+++ project/client/src/services/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
-export const supabaseUrl = 'https://aczenurexbmxuglflosy.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjemVudXJleGJteHVnbGZsb3N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA0NDg5NzUsImV4cCI6MjAzNjAyNDk3NX0.z9-wzoQ8IUH525VRdkcPgTvFYYJ-AmNFC5KO2cNPMf4"
+export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase;
 
--- project/client/vite.config.js
+++ project/client/vite.config.js
@@ -1,12 +1,19 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { loadEnv,defineConfig } from "vite"
 
-export default defineConfig({
+export default defineConfig(({ mode })=>{
+  const env = loadEnv(mode, process.cwd(),"");
+  return{
+  define:{
+    "process.env.REACT_APP_SUPABASE_URL": JSON.stringify(env.REACT_APP_SUPABASE_URL),
+    "process.env.REACT_APP_SUPABASE_ANON_KEY":JSON.stringify(env.REACT_APP_SUPABASE_ANON_KEY),
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+}
 })
